81130e5db8654aa7b024fc1a5b077c0b1bb2d76b
TZdybel <tomek.zdybel@gmail.com>
2018-05-05 02:13:08 +0200

    Komentarze + mała zmiana

--- cw07/zad1/golibroda.c
+++ cw07/zad1/golibroda.c
@@ -38,30 +38,30 @@ void strzyz(struct sembuf *buf) {
     buf[0].sem_op = 0;
     buf[1].sem_num = 2;
     buf[1].sem_op = 1;
-    semop(semid, buf, 2);
+    semop(semid, buf, 2);       //"uzyskuję dostęp" do danych fotela (data[0])
     printf("Zabieram się za strzyżenie klienta o ID: %d\n", data[0]);
     gettime();
     printf("Obstrzygłem klienta o ID: %d\n", data[0]);
     buf[0].sem_num = 2;
     buf[0].sem_op = -1;
-    data[N+4] = 0;
-    data[0] = 0;
+    data[N+4] = 0;      //ustawienie flagi potwierdzenia na 0
+    data[0] = 0;        //zakonczenie strzyżenia
     gettime();
-    semop(semid, buf, 1);
-    while (data[N+4] != 1) {}
-    data[N+4] = 0;
+    semop(semid, buf, 1);   //zwolnienie dostępu
+    while (data[N+4] != 1) {}  //czeka na potwierdzenie przez klienta
+    data[N+4] = 0;   //ustawienie flagi na 0
 }
 
 void spanko(struct sembuf *buf) {
     buf[0].sem_num = 0;
     buf[0].sem_op = -1;
-    semop(semid, buf, 1);
+    semop(semid, buf, 1);   //zwolnienie dostępu do poczekalni
     printf("Idę spać\n");
     gettime();
-    data[N+3] = 1;
+    data[N+3] = 1;          //ustawienie flagi spania na 1
     buf[0].sem_num = 1;
     buf[0].sem_op = -1;
-    semop(semid, buf, 1);
+    semop(semid, buf, 1);   //zwolnienie dostępu do informacji o stanie golibrody
     while (data[N+3] == 1) {} //zasypia
     printf("Budzę się\n");
     gettime();
@@ -76,18 +76,18 @@ int sprawdzPoczekalnie(struct sembuf *buf) {
     buf[2].sem_op = 0;
     buf[3].sem_num = 0;
     buf[3].sem_op = 1;
-    semop(semid, buf, 4);
+    semop(semid, buf, 4);   //uzyskanie dostępu do danych kolejki i stanu golibrody
     printf("Sprawdzam czy jest ktoś w poczekalni\n");
-    if (data[1] != 0) {
+    if (data[1] != 0) {     //jeśli jest ktoś w poczekalni
         //zaproś do strzyżenia
         printf("Zaprosiłem do strzyżenia nastepnego klienta o ID: %d\n", data[1]);
-        for (int i = 0; i < N; ++i) {
+        for (int i = 0; i < N; ++i) {      //kolejka przesuwa się o 1, ostatnie miejsce się zwalnia
             data[i] = data[i+1];
         }
-        while (data[N+4] != 1) {}
         data[N] = 0;
         data[N+2]--;
         gettime();
+        while (data[N+4] != 1) {}          //czeka na potwierdzenie od klienta, na którego przyszła kolej na strzyżenie
         return 1;
     } else return 0;
 }
@@ -99,7 +99,7 @@ void otworzSalon(struct sembuf *buf) {
             buf[0].sem_op = -1;
             buf[1].sem_num = 1;
             buf[1].sem_op = -1;
-            semop(semid, buf, 2);
+            semop(semid, buf, 2);  //zwolnienie dostępu do danych poczekalni i stanie golibrody
             strzyz(buf);
         } else {
             spanko(buf);
@@ -124,13 +124,13 @@ void sigReact(int signum) {
 }
 
 void init() {
-    key_t key = ftok("plik.txt", 4);
+    key_t key = ftok("plik.txt", 4);   //utworzenie klucza
     if (key == -1) {
         printf("Cannot create valid key, does file with this name exists?");
         exit(1);
     }
 
-    semid = semget(key, 3, 0600 | IPC_CREAT);
+    semid = semget(key, 3, 0600 | IPC_CREAT);  //utworzenie zbioru 3 semaforów
     if (semid == -1) {
         printf("Cannot create semaphores\n");
         exit(1);
@@ -143,19 +143,19 @@ void init() {
     initsem.array[2] = 0;  //semafor dający dostęp do info na temat procesu strzyżenia
     semctl(semid, 0, SETALL, initsem);
 
-    shmid = shmget(key, (N+4)*sizeof(int), 0600 | IPC_CREAT);
+    shmid = shmget(key, (N+4)*sizeof(int), 0600 | IPC_CREAT);  //wydzielenie obszaru na pamięć wspólną o wielkości (N+4)*int
     if (shmid == -1) {
         printf("Cannot create shared memory\n");
         exit(1);
     }
 
-    data = (int *)shmat(shmid, NULL, 0);
+    data = (int *)shmat(shmid, NULL, 0);                        //przypisanie pamięci współdzielonej do wskaźnika data
     if (data == (int *)(-1)) {
         printf("Problem with connecting to shared memory\n");
         exit(1);
     }
 
-    for (int i = 0; i < N+5; ++i) data[i] = 0;
+    for (int i = 0; i < N+5; ++i) data[i] = 0;         //inicjalizacja danych w data
     data[N+1] = N;
 }
 
@@ -175,14 +175,14 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    N = (int)strtol(argv[1], NULL, 10);
-
     setSignals();
+
+    N = (int)strtol(argv[1], NULL, 10);
 
     init();
 
-    struct sembuf *buf = (struct sembuf *)calloc(4, sizeof(struct sembuf));
-    for (int i = 0; i < 4; ++i) buf[i].sem_flg = 0;
+    struct sembuf *buf = (struct sembuf *)calloc(4, sizeof(struct sembuf));  //tablica struktur odpowiedzialnych za wykonywanie operacji na semaforach
+    for (int i = 0; i < 4; ++i) buf[i].sem_flg = 0;                          //ustawia flagi na 0
 
     otworzSalon(buf);
 
--- cw07/zad1/klient.c
+++ cw07/zad1/klient.c
@@ -48,8 +48,8 @@ void init() {
         exit(1);
     }
 
-    for (int i = 0;; ++i) {
-        if(data[i] != 0) {
+    for (int i = 0;; ++i) {   //przeszukuje data dopóki nie znajdzie czegoś innego niż 0 - czyli ilości krzeseł w poczekalni
+        if(data[i] != 0) {    //można w bardziej przejrzysty sposób ale nie chciałem zmieniać
             N = data[i];
             break;
         }
@@ -61,7 +61,11 @@ int sprawdzCzySpi(struct sembuf *buf) {
     buf[0].sem_op = 0;
     buf[1].sem_num = 1;
     buf[1].sem_op = 1;
-    semop(semid, buf, 2);
+    buf[2].sem_num = 0;
+    buf[2].sem_op = 0;
+    buf[3].sem_num = 0;
+    buf[3].sem_op = 1;
+    semop(semid, buf, 4);   //uzyskanie dostępu do stanu golibrody i poczekalni
     printf("Sprawdzam czy golibroda śpi - ID: %d\n", (int)getpid());
     gettime();
     return data[N+3];
@@ -73,28 +77,30 @@ void obudzGolibrod(struct sembuf *buf) {
     buf[0].sem_op = 0;
     buf[1].sem_num = 2;
     buf[1].sem_op = 1;
-    semop(semid, buf, 2);
+    semop(semid, buf, 2);   //uzyskuję dostęp do fotela
     printf("Budzę golibrodę - ID: %d\n", (int)getpid());
-    data[N+3] = 0;
+    data[N+3] = 0;          //flaga stanu na obudzonego
     buf[0].sem_num = 1;
     buf[0].sem_op = -1;
+    buf[1].sem_num = 0;
+    buf[1].sem_op = -1;
     gettime();
-    semop(semid, buf, 1);
+    semop(semid, buf, 2);   //oddanie dostępu do stanu golibrody i do poczekalni
 }
 
 void ostrzyzSie(struct sembuf *buf, int version) {
     printf("Siadam na fotelu do strzyżenia - ID: %d\n", (int)getpid());
     if (version == 0) {
-        data[0] = (int)getpid();
+        data[0] = (int)getpid(); //siada na fotelu
         buf[0].sem_num = 2;
         buf[0].sem_op = -1;
         gettime();
-        semop(semid, buf, 1);
+        semop(semid, buf, 1);    //zwolnienie dostępu do fotela
     } else {
         gettime();
     }
-    while(data[0] != 0) {}
-    data[N+4] = 1;
+    while(data[0] != 0) {}   //czeka aż zostanie ostrzyżony
+    data[N+4] = 1;           //wysyła potwierdzenie
     printf("Zostałem ostrzyżony - ID: %d\n", (int)getpid());
     gettime();
 }
@@ -105,29 +111,26 @@ void wyjdz() {
 }
 
 int idzDoPoczekalni(struct sembuf *buf) {
-    buf[0].sem_num = 0;
-    buf[0].sem_op = 0;
-    buf[1].sem_num = 0;
-    buf[1].sem_op = 1;
-    buf[2].sem_num = 1;
-    buf[2].sem_op = -1;
-    semop(semid, buf, 3);
-    if(data[N+2] < data[N+1]) {
-        data[1+data[N+2]] = (int)getpid();
+    buf[0].sem_num = 1;
+    buf[0].sem_op = -1;
+    semop(semid, buf, 1);   //zwolnienie dostępu do stanu golibrody
+    if(data[N+2] < data[N+1]) {   //czy jest miejsce w poczekalni
+        //jeśli tak
+        data[1+data[N+2]] = (int)getpid();  //ustawia się na pierwsze wolne miejsce
         data[N+2]++;
         printf("Zająłem miejsce nr %d w poczekalni - ID: %d\n", data[N+2], (int)getpid());
         gettime();
         buf[0].sem_num = 0;
         buf[0].sem_op = -1;
-        semop(semid, buf, 1);
+        semop(semid, buf, 1);  //zwolnienie dostęp do kolejki
         int pid = (int)getpid();
-        while(data[0] != pid) {}
-        data[N+4] = 1;
+        while(data[0] != pid) {}  //czeka, dopóki nie zostaniesz poproszony przez golibrodę
+        data[N+4] = 1;            //ustawia flagę zrozumienia
         return 0;
     } else {
         buf[0].sem_num = 0;
         buf[0].sem_op = -1;
-        semop(semid, buf, 1);
+        semop(semid, buf, 1);    //zwolnienie dostępu do kolejki
         printf("Brak miejsc. ");
         wyjdz();
         return -1;
@@ -147,14 +150,6 @@ void wejdzDoSalonu(struct sembuf *buf) {
     }
 }
 
-void intReact(int signum) {
-    printf("Kończenie...\n");
-    if(shmdt(data) == -1) {
-        printf("Cannot detach shared memory\n");
-    }
-    exit(1);
-}
-
 int main(int argc, char *argv[]) {
     if(argc != 3) {
         printf("Wrong number of arguments\n");
@@ -165,7 +160,6 @@ int main(int argc, char *argv[]) {
     int numOfLoops = (int)strtol(argv[2], NULL, 10);
 
     init();
-    signal(SIGINT, intReact);
 
     struct sembuf *buf = (struct sembuf *)calloc(4, sizeof(struct sembuf));
     for (int i = 0; i < 4; ++i) buf[i].sem_flg = 0;
@@ -183,7 +177,7 @@ int main(int argc, char *argv[]) {
         } else continue;
     }
 
-    for (int k = 0; k < numOfProcesses; ++k) {
+    for (int k = 0; k < numOfProcesses; ++k) {  //czeka aż wszystkie procesy się zakończą
         wait(NULL);
     }
 
